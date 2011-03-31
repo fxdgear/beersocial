04de29b6bd174410aa584d27534c2aa305ac7d2a
Nick Lang <nicholas@nick-langs-macbook-pro-44.local>
2011-03-31 01:34:27 -0500

    moving files

--- core/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from beersocial.core.models import Challenge
-
-admin.site.register(Challenge)
--- posts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from beersocial.posts.models import Post
-
-admin.site.register(Post)
