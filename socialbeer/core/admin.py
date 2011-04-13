3f2160220d9505fb3727d5f16dbce1ce6a5b5371
Nick Lang <nick.lang@gmail.com>
2011-04-12 20:42:16 -0500

    making the admin for challenges a bit cleaner

--- socialbeer/core/admin.py
+++ socialbeer/core/admin.py
@@ -4,5 +4,9 @@ from socialbeer.core.models import Challenge
 
 class ChallengeAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
+    date_hierarchy = 'start_date'
+    list_display = ('name', 'start_date', 'end_date', 'creator')
+
+
 
 admin.site.register(Challenge, ChallengeAdmin)
