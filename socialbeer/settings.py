23671c820e6b733fc72d6713f789bac73c946869
Nick Lang <nick.lang@gmail.com>
2011-04-12 20:41:59 -0500

    getting static files to serve when debug is false

--- socialbeer/settings.py
+++ socialbeer/settings.py
@@ -52,7 +52,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "media")
+STATIC_ROOT = '/home/nick/code/python/beersocial/src/beersocial/socialbeer/media' 
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -69,6 +69,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     '/home/nick/code/python/beersocial/lib/python2.6/site-packages/django/contrib/admin/media',
+    '/home/nick/code/python/beersocial/src/beersocial/socialbeer/media',
 )
 
 # List of finder classes that know how to find static files in
