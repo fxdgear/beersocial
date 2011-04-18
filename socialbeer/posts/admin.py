f08266f06c7a0f5ccef4ed94766ea3d1aabeb3b3
Nick Lang <nlang@ljworld.com>
2011-04-17 22:55:41 -0500

    adding raw_id_fields to admin

--- socialbeer/posts/admin.py
+++ socialbeer/posts/admin.py
@@ -5,5 +5,6 @@ from socialbeer.posts.models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'author', 'pub_date', 'live')
     list_filter = ('pub_date', 'author')
+    raw_id_fields = ('retweets', 'parent_post', 'author',)
 
 admin.site.register(Post, PostAdmin)
