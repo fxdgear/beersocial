525e16740c6f90523151068038808f4175e121ac
Nick Lang <nlang@ljworld.com>
2011-04-17 22:57:53 -0500

    adding retweet to list_dispaly

--- socialbeer/posts/admin.py
+++ socialbeer/posts/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from socialbeer.posts.models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'author', 'pub_date', 'live')
+    list_display = ('__unicode__', 'author', 'pub_date', 'live', 'retweet',)
     list_filter = ('pub_date', 'author')
     raw_id_fields = ('retweets', 'parent_post', 'author',)
 
